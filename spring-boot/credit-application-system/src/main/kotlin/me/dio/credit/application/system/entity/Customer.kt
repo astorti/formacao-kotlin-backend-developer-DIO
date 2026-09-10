de9f0c5d4158396bba531a980e8e301688e72ac0
André Storti Andrade <a.storti@hotmail.com>
2026-09-10 17:44:55 -0300

    implementação da camada controller

--- spring-boot/credit-application-system/src/main/kotlin/me/dio/credit/application/system/entity/Customer.kt
+++ spring-boot/credit-application-system/src/main/kotlin/me/dio/credit/application/system/entity/Customer.kt
@@ -11,6 +11,7 @@ import jakarta.persistence.GenerationType
 import jakarta.persistence.Table
 import jakarta.persistence.Id
 import jakarta.persistence.OneToMany
+import java.math.BigDecimal
 
 
 @Entity
@@ -20,6 +21,7 @@ data class Customer (
     @Column(nullable = false) var lastName: String = "",
     @Column(nullable = false, unique = true) var cpf: String = "",
     @Column(nullable = false, unique = true) var email: String = "",
+    @Column(nullable = false) var income: BigDecimal = BigDecimal.ZERO,
     @Column(nullable = false) var password: String = "",
     @Embedded var address: Address = Address(),
     @OneToMany(fetch = FetchType.LAZY,
